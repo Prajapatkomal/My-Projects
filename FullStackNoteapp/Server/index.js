f2da5fa2f4de69eb5beeceff2627b45e8ce6f304
Prajapatkomal <prajapatkomal911@gmail.com>
2024-08-22 16:37:02 +0530

    minor changes occured 3rd time

--- FullStackNoteapp/Server/index.js
+++ FullStackNoteapp/Server/index.js
@@ -6,12 +6,12 @@ import dotenv from "dotenv"
 dotenv.config()
 import cors from 'cors'
 
+const PORT = process.env.PORT || 5000
+const app = express()
+
 app.use(cors({
     origin:"*"
 }))
-
-const PORT = process.env.PORT || 5000
-const app = express()
 
 app.use(express.json())
 
